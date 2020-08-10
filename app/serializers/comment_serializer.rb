ad778d9b703f44efba86044a5e1307eef0d3bccc
github username <github email address>
2020-08-10 12:39:16 -0400

    send username in comment response

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :text, :user_id
+  attributes :id, :text, :user_id, :username
+  belongs_to :user
+
+  def username
+    user = User.find_by(id: object.user_id)
+    user.username
+  end
 end
