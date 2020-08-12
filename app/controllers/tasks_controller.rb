d46ad8ff1b4f361a558c0a9f3eff691a5836acf4
github username <github email address>
2020-08-12 18:28:52 -0400

    beginning error handling for task create/update membership create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,7 +19,11 @@ class TasksController < ApplicationController
 
     # category = Category.find_by(name: params[:category])
     task = Task.create(name: params[:name], description: params[:description], created_by: @current_user.username, category_id: params[:category_id], due_date: params[:due_date] )
-    render json: task
+    if task.valid?
+      render json: task
+    else 
+      render json: { "status": "error", "error": task.errors.full_messages.join(" ")}
+    end
   end
 
   def update
@@ -27,7 +31,12 @@ class TasksController < ApplicationController
 
     # category = Category.find_by(name: params[:category])
     task.update(name: params[:name], description: params[:description], category_id: params[:category_id], due_date: params[:due_date] )
-    render json: task
+
+    if task.valid?
+      render json: task
+    else 
+      render json: { "status": "error", "error": task.errors.full_messages.join(" ")}
+    end
 
 
   end
